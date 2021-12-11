3dbbd88febfb0a8270bc246e4152f28b31edb4d1
TeddyJack <thedorbastard@mail.ru>
2021-12-11 14:17:02 +0300

    Clean the code, optimize

--- secret_santa.py
+++ secret_santa.py
@@ -1,15 +1,10 @@
-import random
-import datetime
+from random import shuffle
 
 with open('names.txt', encoding='utf-8') as f_in:
     names = f_in.read().splitlines()
-
-random.shuffle(names)
+    shuffle(names)
 
 with open('pairs.txt', 'w', encoding='utf-8') as f_out:
-    f_out.write('Пары составлены: {}\n\n'.format(datetime.datetime.now()))
-    line = ''
-    for i in range(len(names)):
-        next_idx = 0 if i == len(names) - 1 else i + 1
-        line = '{} -> {}\n'.format(names[i], names[next_idx])
-        f_out.write(line)
+    for i in range(len(names) - 1):
+        f_out.write(f'{names[i]} -> {names[i+1]}\n')
+    f_out.write(f'{names[len(names) - 1]} -> {names[0]}\n')
